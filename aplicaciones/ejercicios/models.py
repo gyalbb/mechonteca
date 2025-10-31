b9690d160530774cb2c56950745a19f544b81c5d
prettyhatemvchine <100millonesfueronpocos@gmail.com>
2025-10-30 23:42:19 -0300

    avances en app de preguntas (wip)

--- aplicaciones/ejercicios/models.py
+++ aplicaciones/ejercicios/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
+class Ejercicios(models.Model):
+    enunciado = models.CharField(max_length=255)
+    ejercicio = models.CharField(max_length=255)
+    respuesta_correcta = models.CharField(max_length=255)
+    alternativa1 = models.CharField(max_length=255)
+    alternativa2 = models.CharField(max_length=255)
+    alternativa3 = models.CharField(max_length=255)
+    topico = models.CharField(max_length=255)
+
 # Create your models here.
