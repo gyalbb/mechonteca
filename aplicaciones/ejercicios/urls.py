beeab5477c5cbd36f036514f15910208e239b025
prettyhatemvchine <100millonesfueronpocos@gmail.com>
2025-11-01 20:32:23 -0300

    avances en app de pruebas

--- aplicaciones/ejercicios/urls.py
+++ aplicaciones/ejercicios/urls.py
@@ -4,5 +4,7 @@ from . import views
 app_name = 'ejercicios'
 
 urlpatterns = [
-    path('', views.ejercicios, name='ejercicios'),
+    path('', views.index, name='index'),
+    path('ejemplo_ejercicios/', views.ejercicios, name='ejercicios'),
+    path('crear-prueba/', views.crear_prueba, name="crear-prueba")
 ]
--- aplicaciones/ejercicios/views.py
+++ aplicaciones/ejercicios/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
-from .models import Ejercicios
+from .models import Alternativas, Ejercicios, Preguntas, Pruebas
 import random
+
+def index(request):
+    return render(request, 'index.html')
 
 # Create your views here.
 def ejercicios(request):
@@ -22,3 +25,39 @@ def ejercicios(request):
     }
 
     return render(request, 'ejercicios.html', contexto)
+
+def crear_prueba(request):
+    usuario = request.POST.get('username')
+    ejercicios_todos = list(Ejercicios.objects.all())
+    seleccion_ejercicios = random.sample(ejercicios_todos, 15)
+    prueba = Pruebas.objects.create(usuario=usuario)
+    num_pregunta = 1
+    preguntas_template = list()
+    for ejercicio in seleccion_ejercicios:
+        pregunta = Preguntas.objects.create(prueba=prueba, ejercicio=ejercicio, num_pregunta=num_pregunta)
+        opciones = [ejercicio.respuesta_correcta, ejercicio.alternativa1, ejercicio.alternativa2]
+        random.shuffle(opciones)
+        orden = 1
+        alternativas = list()
+        for opcion in opciones:
+            Alternativas.objects.create(pregunta=pregunta, orden=orden, formula=opcion)
+            alternativas.append({
+                'orden': orden,
+                'formula': opcion
+            })
+            orden += 1
+        
+        preguntas_template.append({
+            'enunciado': ejercicio.enunciado,
+            'ejercicio': ejercicio.ejercicio,
+            'num_pregunta': pregunta.num_pregunta,
+            'alternativas': alternativas,
+            'topico': ejercicio.topico
+        })
+        num_pregunta += 1
+
+    contexto = {
+        'prueba': prueba,
+        'preguntas': preguntas_template,
+    }
+    return render(request, 'crear-prueba.html', contexto)
