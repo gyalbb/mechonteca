68846d95ca120fa36f56e9a8a88c076ca70e9ef7
gyalbb <angela.ibacache11@gmail.com>
2025-11-11 20:57:15 -0300

    Modernizacion UI templates y mejoras visuales

--- config/settings.py
+++ config/settings.py
@@ -120,6 +120,12 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+# Include project-level 'static' directory so root assets like
+# 'static/aplicaciones/img/*' are served in development.
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
