acbc1e815c872885f5303a621d82f80e59839fb1
Estudiante USM <estudiante@example.com>
2025-11-14 14:47:13 -0300

    Import dashboard from Proyecto_conectado_v2 into Pagina_v2

--- aplicaciones/dashboard/urls.py
+++ aplicaciones/dashboard/urls.py
@@ -7,5 +7,4 @@ urlpatterns = [
     path('', views.dashboard_view, name='dashboard_view'),
     path('diagnostico/<slug:asignatura_slug>/', views.diagnostico_view, name='diagnostico_view'),
     path('formulas/', views.formulas_view, name='formulas_view'),
-    path('ejercitar/', views.ejercitar_view, name='ejercitar_view'),
 ]
